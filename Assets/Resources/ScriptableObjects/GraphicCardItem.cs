8ec74cee4cac2674d5be891bdc67550f52a37029
Sma1lboy <541898146chen@gmail.com>
2023-12-29 18:57:13 -0600

    feat:ScriptableItem
    adding Scriptable Item to easily create Graphic Card instance and help
    manage relate to ItemManager

---
+++ Assets/Resources/ScriptableObjects/GraphicCardItem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class <c>GraphicCardItem</c> basic item in our game
+/// </summary>
+[CreateAssetMenu(fileName = "NewGraphicCard", menuName ="ScriptableObjects/GraphicCardItem")]
+public class GraphicCardItem : ScriptableObject
+{
+    public string Name;
+    public string Id;
+    public Sprite Icon;
+    public bool IsLocked;
+    public double PerSecondEarn;
+    public double Price;
+    public double PerSecondLoseVolt;
+    public int Quantity;
+}
