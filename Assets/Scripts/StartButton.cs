f4bd6a156c5bbd51ccd2e6ea2b1d15b6fba37af4
NarwhalChen <spixel.xi@gmail.com>
2023-12-30 17:13:27 -0600

    to: start menu
    create basic start menu and start button

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class StartButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(ButtonClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void ButtonClick()
+    {
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene("Main");
+    }
+
+   
+}
