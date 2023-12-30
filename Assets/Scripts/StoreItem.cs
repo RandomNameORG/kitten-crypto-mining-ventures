61a1fdb9dd8a41efb5cb81b87583467a5c32f09b
Sma1lboy <541898146chen@gmail.com>
2023-12-29 19:00:09 -0600

    feat:StoreItem

---
+++ Assets/Scripts/StoreItem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreItem : MonoBehaviour
+{
+    public SpriteRenderer Renderer;
+    public TextMesh text
+}
