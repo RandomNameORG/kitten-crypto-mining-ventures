1406dcdd01709e4adb4c92cf45c238d326bcd9b2
Sma1lboy <541898146chen@gmail.com>
2023-12-29 18:59:36 -0600

    feat:ItemManager

---
+++ Assets/Scripts/ItemManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using static UnityEditor.Progress;
+using System.Linq;
+/// <summary>
+/// singalton item manager
+/// </summary>
+public class ItemManager : MonoBehaviour
+{
+    public static ItemManager Instance;
+
+    public GraphicCardItem[] GraphicCardItems;
+
+    /// <summary>
+    /// Awake is called when script instance is being loaded, which means
+    /// it will init when game start if we put it on helper object
+    /// </summary>
+    private void Awake()
+    {
+        Debug.Log("Item Manager init...");
+        Instance = this;
+        GraphicCardItems = Utils.GetAllInstance<GraphicCardItem>();
+        GraphicCardItems = GraphicCardItems.OrderBy(item => item.Id).ToArray();
+        foreach(GraphicCardItem item in GraphicCardItems)
+        {
+            Debug.Log(item);
+        }
+
+        Debug.Log("Item Manager Done!s");
+    }
+}
