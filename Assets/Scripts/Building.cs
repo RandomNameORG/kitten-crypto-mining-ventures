05e31c0ec00cc4033199038c0f71c90f666c40ea
Sma1lboy <541898146chen@gmail.com>
2023-12-30 17:49:49 -0600

    feat<OOD>: adding player, building, event model to proj

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Building : ScriptableObject
+{
+    public string Id;
+    public long Capacity;
+    public List<GeneralEvent> Events;
+    public List<GraphicCardItem> GraphicCards;
+    public double EventHappenProbs;
+}
+
