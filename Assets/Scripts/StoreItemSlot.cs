50c7f381e64e5b878e0120be96774db196e8ea2f
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2023-12-31 03:46:55 -0600

    feat: StoreMenuUI adn slot
    adding StoreMenuUI script for monitor all StoreItemSlot, help init all slots
    adding StoreItemSlot to encapsulation gereral item(GraphicCardItem), image, MoneyText

---
+++ Assets/Scripts/StoreItemSlot.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+using static UnityEditor.Progress;
+
+public class StoreItemSlot : MonoBehaviour
+{
+	GraphicCardItem Item;
+	public Image Icon;
+	public TextMeshProUGUI MoneyText;
+	public Button Button;
+	Player _player;
+	public void AddItem(GraphicCardItem item)
+	{
+		_player = Player.Instance;
+		this.Item = item	;
+		this.Icon.sprite = this.Item.Icon;
+		this.Icon.enabled = true;
+        MoneyText.text = "$ "+Item.Price + "";
+        MoneyText.enabled = true;
+
+		//when click the item, bought it!
+		Button.onClick.AddListener(OnBuy);
+
+    }
+	public void ClearSlot()
+	{
+		Item = null;
+		Icon.enabled = false;
+		Icon.sprite = null;
+        MoneyText.enabled = false;
+        MoneyText.text = "";
+
+    }
+    // Use this for initialization
+    void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+			
+	}
+	void OnBuy()
+	{
+		if(_player.Money < Item.Price)
+        {
+			//TODO no money you can't buy
+			Debug.Log("no money");
+			return;
+		}
+		Building building = _player.currBuildingAt;
+
+		Debug.Log(building.Capacity);
+        Debug.Log(building.CardSize());
+
+        if (building.Capacity < building.CardSize())
+		{
+            //todo no mroe capacity
+            Debug.Log("no capacity");
+            return;
+		}
+		_player.currBuildingAt.AddingGraphicCard(this.Item);
+		_player.Money -= this.Item.Price;
+		//TODO animation loading decreasing value smoothing papaap
+	}
+
+}
+
---
+++ Assets/Scripts/StoreMenuUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+public class StoreMenuUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public StoreItemSlot[] Slots;
+    ItemManager _itemManager;
+    public GameObject originSlot;
+    void Start()
+    {
+        _itemManager = ItemManager.Instance;
+        Transform slotGridTras = transform.Find("StoreItemSlotGrid");
+        Slots = transform.GetComponentsInChildren<StoreItemSlot>();
+        UpdateUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void UpdateUI()
+    {
+        GraphicCardItem[] items = _itemManager.GraphicCardItems;
+
+        for (int i = 0; i < _itemManager.GraphicCardItems.Length; i++)
+        {
+            Slots[i].AddItem(items[i]);
+        }
+    }
+}
