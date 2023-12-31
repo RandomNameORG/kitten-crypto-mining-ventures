d90b5042fc6124e8fb4df9a6685adea3e98a0689
Sma1lboy <541898146chen@gmail.com>
2023-12-31 00:29:21 -0600

    feat<manager>: Singleton building manager

---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class BuildingManager : MonoBehaviour
+{
+	public static BuildingManager Instance;
+	public Building[] Buildings;
+    // Use this for initialization
+    private void Awake()
+    {
+		Instance = this;
+
+        Debug.Log("Building Manager init...");
+
+        Buildings = Utils.GetAllInstance<Building>();
+        foreach (Building building in Buildings)
+        {
+            Debug.Log(building);
+        }
+        Debug.Log("Building Manager done");
+    }
+    void Start()
+    {
+
+    }
+
+    // Read: Find a Building by its ID
+    public Building FindBuildingById(string id)
+    {
+        return Buildings.FirstOrDefault(building => building.Id.Equals(id));
+    }
+    // Read: Find a Building by its name
+    public Building FindBuildingByName(string name)
+    {
+        return Buildings.FirstOrDefault(building => building.Name.Equals(name));
+    }
+}
+
+
