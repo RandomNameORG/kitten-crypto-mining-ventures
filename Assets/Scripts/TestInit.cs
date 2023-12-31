630b40cfa24182834a33c74ccc47941b08019149
Sma1lboy <541898146chen@gmail.com>
2023-12-31 00:29:48 -0600

    build<test>: test script

---
+++ Assets/Scripts/TestInit.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Test init class for this proj, setup some demo builiding and graphic card
+/// </summary>
+public class TestInit : MonoBehaviour
+{
+	
+	// Use this for initialization
+	void Start()
+	{
+		Player player = Player.Instance;
+        BuildingManager buildingManager = BuildingManager.Instance;
+        Building demoRoom = buildingManager.FindBuildingByName("DemoRoom");
+		ItemManager itemManager = ItemManager.Instance;
+		GraphicCardItem card =  itemManager.FindGraphicCardItemByName("GTX1060");
+		for(int i = 0; i < 1; i++)
+		{
+			demoRoom.AddingGraphicCard(card);
+			Debug.Log(card  + " and " + demoRoom + " correct");
+		}
+		player.Buildings.Add(demoRoom);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+			
+	}
+}
+
