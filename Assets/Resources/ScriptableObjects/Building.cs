c62b8792129b2c859504d4642bb306deca2cafe8
NarwhalChen <spixel.xi@gmail.com>
2023-12-31 02:14:08 -0600

    to: Building
    Add alternator and voltage related method

--- Assets/Resources/ScriptableObjects/Building.cs
+++ Assets/Resources/ScriptableObjects/Building.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using AlternatorProject;
 
 [CreateAssetMenu(fileName = "NewBuilding", menuName = "ScriptableObjects/Buildings")]
 public class Building : ScriptableObject
@@ -12,7 +13,9 @@ public class Building : ScriptableObject
     public List<GraphicCardItem> Cards;
     public double EventHappenProbs;
     public long MoneyPerSecond;
-    public long VoltPerSecond;
+    public  List<Alternator> alts;
+    public long MaxVolt = 0;
+    public long VoltPerSecond = 0;
 
     public void AddingGraphicCard(GraphicCardItem card)
     {
@@ -26,7 +29,7 @@ public class Building : ScriptableObject
         this.MoneyPerSecond += card.PerSecondEarn;
         this.VoltPerSecond += card.PerSecondLoseVolt;
     }
-    public bool RemoveGraphicCard(GraphicCardItem card)
+    public bool  RemoveGraphicCard(GraphicCardItem card)
     {
         if (Cards.Contains(card))
         {
@@ -40,6 +43,29 @@ public class Building : ScriptableObject
             Debug.LogError("Card not found in " + Id);
             return false;
         }
+    }
+
+    public void AddingAlternator(Alternator alternator){
+        alts.Add(alternator);
+        MaxVolt += alternator.MaxVolt;
+
+    }
+    public bool RemoveAlternator(Alternator alternator){
+
+        if(alts.Contains(alternator)){
+             if(VoltPerSecond - alternator.MaxVolt < 0){
+                 Debug.LogError("Power Failure in " + Id);
+             }else{
+                 alts.Remove(alternator);
+                 MaxVolt -= alternator.MaxVolt;
+             }
+             return true;
+        }else{
+            Debug.LogError("Alternator not found in " + Id);
+            return false;
+        }
+        
+
     }
 }
 
