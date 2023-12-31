48580fc8a8a84f5a39b4d025d422828223ecc6f0
NarwhalChen <spixel.xi@gmail.com>
2023-12-30 19:16:26 -0600

    to:Setting
    Add setting page

---
+++ Assets/Scripts/ExitButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ExitButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(ButtonClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void ButtonClick()
+    {
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
