223763f1ba0f0ef7ea0657298166445a9ebe8ed0
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-01-08 23:48:59 -0800

    feat: SettingWindow
    Adding basic setting window

---
+++ Assets/SettingsWindow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsWindow : MonoBehaviour
+{
+
+    /// <summary>
+    /// we should have a lot properties here
+    /// we should keep tracking all the properties
+    /// if properties change, we have to tell user to apply new one or reset it 
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
