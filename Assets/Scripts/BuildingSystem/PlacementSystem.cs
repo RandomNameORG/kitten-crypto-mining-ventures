aa4e12488bcd2afed9c42fde614972fd0342f20d
NarwhalChen <spixel.xi@gmail.com>
2024-01-09 19:21:56 -0500

    to: GridPlacement
    Add gridplacement function to storage

---
+++ Assets/Scripts/BuildingSystem/PlacementSystem.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementSystem : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mouseIndicator, cellIndicator;
+    [SerializeField]
+    private inputManager inputManager;
+    [SerializeField]
+    private Grid grid;
+
+    [SerializeField]
+    private ObjectDataBaseSO database;
+    private int selectedObjectIndex = -1; 
+
+    Transform canvasTransform;
+
+    // [SerializeField]
+    // private GameObject gridVisualization;
+
+    public void Start(){
+        StopPlacement();
+        // floorData = new();
+        // furnitureData = new();
+        // previewRender  =  cellIndicator.GetComponentInChildren<Render>();
+    }
+    public void startPlacement(int ID){ 
+        Transform canvasTransform = GameObject.Find("Canvas").transform;
+        StopPlacement();
+        selectedObjectIndex = database.Objects.FindIndex(data => data.ID == ID);    
+        Debug.Log(ID);
+        Debug.Log(selectedObjectIndex);
+        if(selectedObjectIndex < 0){
+            
+            return;
+        }
+        //gridVisualization.SetActive(true);
+        cellIndicator.SetActive(true);
+        inputManager.OnClicked += PlaceStructure;
+        inputManager.OnExit += StopPlacement;
+       
+    }
+    private void StopPlacement(){
+        selectedObjectIndex = -1;
+        //gridVisualization.SetActive(false);
+        //cellIndicator.SetActive(false);
+        inputManager.OnClicked -= PlaceStructure;
+        inputManager.OnExit -= StopPlacement;
+    }
+    private void PlaceStructure(){
+        if(inputManager.IsPointerOverUI()){ 
+            Debug.Log("return");
+            return;
+        }
+        
+        Vector2 mousePosition = inputManager.StartPosition();
+        Vector3Int gridPosition = grid.WorldToCell(new Vector3(mousePosition.x, mousePosition.y, 0f));
+
+        GameObject gameobject = Instantiate(database.Objects[selectedObjectIndex].Prefab);
+        gameobject.transform.SetParent(null, true);
+        gameobject.transform.position = grid.GetCellCenterWorld(gridPosition);
+        // Set the Canvas as the parent of the instantiated object
+
+    }
+    public void Update(){
+        
+        
+        if(selectedObjectIndex < 0){
+            return;
+        }
+        Vector2 mousePosition = inputManager.StartPosition();
+        Vector3Int gridPosition = grid.WorldToCell(new Vector3(mousePosition.x, mousePosition.y, 0f));
+        mouseIndicator.transform.position = mousePosition;
+        cellIndicator.transform.position = grid.GetCellCenterWorld(gridPosition);
+    }
+}
---
+++ Assets/Scripts/BuildingSystem/inputManager.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class inputManager : MonoBehaviour
+{
+    [SerializeField]
+    private Camera sceneCamera;
+
+    private Vector3 lastPosition;
+
+    [SerializeField]
+    private LayerMask placementLayermask;
+
+    public event Action OnClicked, OnExit;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+            OnClicked?.Invoke();
+        if(Input.GetKeyDown(KeyCode.Escape))
+            OnExit?.Invoke();
+    }
+
+    public bool IsPointerOverUI()
+        => EventSystem.current.IsPointerOverGameObject();
+
+    public Vector2 StartPosition(){
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        return mousePos;
+    }
+    public Vector2 GetSelectedMapPosition(){
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero, 0f, placementLayermask);
+        if(hit.collider != null){
+            lastPosition = hit.point;
+        }
+        return lastPosition;
+    }
+}
