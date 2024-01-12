2705b98f3f2343363fff86f461b32e419ad1291b
NarwhalChen <spixel.xi@gmail.com>
2024-01-12 01:55:22 -0500

    to: BuildingEntry
    Adding some new data in the building

--- Assets/Scripts/BuildingEntry.cs
+++ Assets/Scripts/BuildingEntry.cs
@@ -1,20 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BuildingEntry : MonoBehaviour
+[Serializable]
+public class BuildingList{
+    public List<BuildingEntry> Buildings;
+}
+
+[Serializable]
+public class BuildingEntry
 {
-    public int GridSize = 7; // 7 grids
+    public int GridSize;
+
     public List<Decoration> Decorations;
-    public Resorces RoomMaterial;
+
+    public List<Cat> Cats;
+
+    public Resorces BuildingMaterial;
 
     public int VoltPerSecond;
+
     public int MaxVolt;
 
     public int MaxCardNum;
 
+    public int ProbabilityOfBeingAttacked;
 
-    
+    public int HeatDissipationLevel;
+
+    public int LocationOfTheBuilding;
 }
 
 public class Decoration
@@ -22,6 +37,13 @@ public class Decoration
     public int ID;
     public GameObject Prefab;
     public float[] Coordinates;
+}
+
+public class Cat
+{
+    public int ID;
+    public string Name;
+    //TODO: skills and so on
 }
 
 public class Resorces
