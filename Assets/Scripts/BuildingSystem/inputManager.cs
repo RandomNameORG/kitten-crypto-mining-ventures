6a4d6722a81255c2d627fd37ccff087efb1f8b11
NarwhalChen <spixel.xi@gmail.com>
2024-01-11 23:51:04 -0500

    fix: fix variable name
    fix variable name to follow csharp variable principle

--- Assets/Scripts/BuildingSystem/inputManager.cs
+++ Assets/Scripts/BuildingSystem/inputManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class inputManager : MonoBehaviour
+public class InputManager : MonoBehaviour
 {
     [SerializeField]
     private Camera sceneCamera;
