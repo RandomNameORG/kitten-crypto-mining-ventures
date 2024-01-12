58f9eb7dfb2837432c840a7161766201cc6e2ec6
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-01-12 01:38:28 -0800

    bug: testinit no need anymore

--- Assets/Scripts/TestInit.cs
+++ Assets/Scripts/TestInit.cs
@@ -6,32 +6,32 @@ using System.Collections;
 /// </summary>
 public class TestInit : MonoBehaviour
 {
-	
+
 	// Use this for initialization
 	void Start()
 	{
-		
-		Player player = Player.Instance;
-		player.Money = 2;
-        BuildingManager buildingManager = BuildingManager.Instance;
-        Building demoRoom = buildingManager.FindBuildingByName("DemoRoom");
 
-		ItemManager itemManager = ItemManager.Instance;
-		GraphicCardItem card =  itemManager.FindGraphicCardItemByName("GTX1060");
-		for(int i = 0; i < 1; i++)
-		{
-			demoRoom.AddingGraphicCard(card);
-			Debug.Log(card  + " and " + demoRoom + " correct");
-		}
+		// Player player = Player.Instance;
+		// player.Money = 2;
+		// BuildingManager buildingManager = BuildingManager._instance;
+		// Building demoRoom = buildingManager.FindBuildingByName("DemoRoom");
 
-		player.currBuildingAt = demoRoom;
-		player.Buildings.Add(demoRoom);
+		// ItemManager itemManager = ItemManager.Instance;
+		// GraphicCardItem card = itemManager.FindGraphicCardItemByName("GTX1060");
+		// for (int i = 0; i < 1; i++)
+		// {
+		// 	demoRoom.AddingGraphicCard(card);
+		// 	Debug.Log(card + " and " + demoRoom + " correct");
+		// }
+
+		// player.currBuildingAt = demoRoom;
+		// player.Buildings.Add(demoRoom);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-			
+
 	}
 }
 
