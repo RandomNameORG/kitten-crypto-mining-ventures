877b383284264e75cedd1f7c1d00d0fd1eff41f1
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-01-12 23:54:10 -0800

    feat: Building class, json class, ref class, and data class

---
+++ Assets/Scripts/Models/Building.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using AlternatorProject;
+using DG.Tweening;
+using TMPro;
+
+/// <summary>
+/// The object class to attch with gameobject   
+/// </summary>
+public class Building : MonoBehaviour
+{
+    public string Id;
+    public string Name;
+    public long Capacity;
+    public List<GeneralEvent> Events;
+    public List<GraphicCard> Cards;
+    public double EventHappenProbs;
+    public long MoneyPerSecond;
+    public List<Alternator> Alts;
+    public long MaxVolt;
+    public long VoltPerSecond;
+    public static Text VoltText;
+
+
+    public void AddingGraphicCard(GraphicCard card)
+    {
+        this.Cards.Add(card);
+        this.MoneyPerSecond += card.PerSecondEarn;
+        this.VoltPerSecond += card.PerSecondLoseVolt;
+    }
+    public bool RemoveGraphicCard(GraphicCard card)
+    {
+        if (Cards.Contains(card))
+        {
+            Cards.Remove(card);
+            MoneyPerSecond -= card.PerSecondEarn;
+            VoltPerSecond -= card.PerSecondLoseVolt;
+            return true;
+        }
+        else
+        {
+            Debug.LogError("Card not found in " + Id);
+            return false;
+        }
+    }
+    public int CardSize() { return this.Cards.Count; }
+
+    public void AddingAlternator(Alternator alternator)
+    {
+        Alts.Add(alternator);
+        MaxVolt += alternator.MaxVolt;
+
+    }
+    public bool RemoveAlternator(Alternator alternator)
+    {
+
+        if (Alts.Contains(alternator))
+        {
+            if (VoltPerSecond - alternator.MaxVolt < 0)
+            {
+                Debug.LogError("Power Failure in " + Id);
+            }
+            else
+            {
+                Alts.Remove(alternator);
+                MaxVolt -= alternator.MaxVolt;
+            }
+            return true;
+        }
+        else
+        {
+            Debug.LogError("Alternator not found in " + Id);
+            return false;
+        }
+
+
+    }
+
+
+
+
+}
+
---
+++ Assets/Scripts/Models/BuildingEntry.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using AlternatorProject;
+using UnityEngine;
+
+[Serializable]
+public class BuildingEntryList
+{
+    public List<BuildingEntry> Buildings = new List<BuildingEntry>();
+}
+
+[Serializable]
+public class BuildingReference
+{
+    public string Id;
+    public string Name;
+}
+/// <summary>
+/// Building Json Data class
+/// </summary>
+[Serializable]
+public class BuildingEntry
+{
+    public int Id;
+    public string Name;
+    public int GridSize;
+    public Resorces BuildingMaterial;
+
+    public long VoltPerSecond;
+    public long MoneyPerSecond; //Money earn persecond
+    public long MaxVolt;
+
+    public long MaxCardNum;
+
+    public int ProbabilityOfBeingAttacked;
+
+    public int HeatDissipationLevel;
+
+    public int LocationOfTheBuilding;
+    public List<Decoration> Decorations;
+
+    public List<Cat> Cats;
+
+    //this is the card info we need to store
+    public List<GraphicCardReference> CardSlots;
+
+    //TODO We cannot store entity data for these, each only needs a little bit of data
+    //For example, the following three data should not be stored in json
+    //using NonSerialized like this for all of them. otherwise, it will apear on json file
+
+    public List<GeneralEvent> Events;
+    public List<GraphicCard> Cards;
+    public List<Alternator> alts;
+}
+
+
+
+[Serializable]
+public class Decoration
+{
+    public int ID;
+    public GameObject Prefab;
+    public float[] Coordinates;
+}
+
+[Serializable]
+public class Cat
+{
+    public int ID;
+    public string Name;
+    //TODO: skills and so on
+}
+
+[Serializable]
+public class Resorces
+{
+
+#nullable enable
+    public string? RightWallMaterial;
+    public string? LeftWallMaterial;
+
+    public string? RightFloorMaterial;
+    public string? LeftFloorMaterial;
+
+}
