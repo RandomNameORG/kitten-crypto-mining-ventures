2850c3f895773e1c6b5eb4fbc4c493fff2efd9a1
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-01-16 16:28:09 -0800

    bugs: delete namespace for Alternator

--- Assets/Resources/ScriptableObjects/Alternator.cs
+++ Assets/Resources/ScriptableObjects/Alternator.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace AlternatorProject
+
+public class Alternator : MonoBehaviour
 {
-    public class Alternator : MonoBehaviour
-    {
-        public int AlternatorId = 0;
-        public string Name = "Basic Alternator";
-        public long MaxVolt = 1000;
-    }
+    public int AlternatorId = 0;
+    public string Name = "Basic Alternator";
+    public long MaxVolt = 1000;
 }
+
--- Assets/Scripts/Managers/BuildingManager.cs
+++ Assets/Scripts/Managers/BuildingManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Linq;
 using System.Collections.Generic;
 using System.IO;
-using AlternatorProject;
 using System;
 
 
@@ -30,67 +29,16 @@ public class BuildingManager : MonoBehaviour
     //Mention: before you starting code your loading data, you have to create init a file first
     void Start()
     {
-        Logger.Log(LogType.INIT);
         //get json data
-        _building_entries = DataLoader.LoadData<BuildingEntryList>(DataType.BuildingData);
-        ;
-
-        //TODO we need to make this process more easy to write, this way so stupid...
-        //or pust some where else
-        //convert to building object list
-        _building_entries.Buildings.ForEach(e =>
-        {
-
-            var obj = new GameObject(e.Name);
-            //create building comp
-            obj.AddComponent<Building>();
-            var building = obj.GetComponent<Building>();
-            building.Id = e.Id.ToString(); // Assuming  convert the int Id to string
-            building.Name = e.Name;
-            building.Capacity = e.MaxCardNum;
-            building.MaxVolt = e.MaxVolt; // Assuming Capacity is equivalent to MaxVolt
-            building.Events = new List<GeneralEvent>(e.Events);
-            building.Cards = e.CardSlots.Select(cs =>
-            {
-                return GraphicCardManager._instance.FindCardById(cs.Id);
-            }).ToList();
-            building.EventHappenProbs = e.ProbabilityOfBeingAttacked;
-            building.MoneyPerSecond = e.MoneyPerSecond;
-            building.Alts = new List<Alternator>(e.alts);
-            building.VoltPerSecond = e.VoltPerSecond;
-            buildings.Add(building);
-            Buildings.Add(obj);
-
-            //here we have to do something to building house
-            //make it exist in the world
-            obj.transform.SetPositionAndRotation(new Vector3(0, 0, 0), Quaternion.identity);
-            obj.transform.localScale = new Vector3(1, 1, 1);
-        });
+        _building_entries = DataManager._instance.GetData<BuildingEntryList>(DataType.BuildingData);
+        var data = DataMapper.BuildingJsonToData(_building_entries);
+        buildings = data.buildings; //Building class
+        Buildings = data.Buildings; //the actual GameObject holding building comp
         Logger.Log(LogType.INIT_DONE);
     }
     private void OnApplicationQuit()
     {
-        _building_entries = new BuildingEntryList();
-        //TODO optimaztion this process plz
-        Buildings.ForEach(obj =>
-        {
-            var building = obj.GetComponent<Building>();
-            BuildingEntry e = new BuildingEntry();
-            e.Id = building.Id;
-            e.Name = building.Name;
-            e.MaxCardNum = building.Capacity; // Assuming MaxCardNum is equivalent to Capacity
-            e.MaxVolt = building.MaxVolt; // Assuming this assignment logic remains the same
-            // Assuming e has a CardSlots property that can be assigned from building.Cards
-            e.CardSlots = building.Cards.Select(gc =>
-            {
-                return new GraphicCardReference { Id = gc.Id, Name = gc.Name }; // Assuming CardSlot has an Id property and you can create new instances like this
-            }).ToList();
-            e.ProbabilityOfBeingAttacked = building.EventHappenProbs;
-            e.MoneyPerSecond = building.MoneyPerSecond;
-            e.VoltPerSecond = building.VoltPerSecond;
-            _building_entries.Buildings.Add(e);
-        });
-        DataLoader.SaveData<BuildingEntryList>(DataType.BuildingData, _building_entries);
+        DataMapper.BuildingDataToJson(_building_entries, Buildings);
     }
 
 
