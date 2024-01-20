93ea4f450918c86d207dd74744a4b7045fae5e6c
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-01-19 19:09:05 -0800

    to : delete some Chinese comment

--- Assets/Scripts/AvoidDestroy.cs
+++ Assets/Scripts/AvoidDestroy.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This class prevent some fixed game object destroy by changing scene
+/// </summary>
 public class AvoidDestroy : MonoBehaviour
 {
     // Start is called before the first frame update
--- Assets/Scripts/Resctricion.cs
+++ Assets/Scripts/Resctricion.cs
@@ -5,14 +5,11 @@ using UnityEngine;
 public class Resctricion : MonoBehaviour
 {
     private Collider2D myCollider;
-    public GameObject buildingSystem; // 指定你要隐藏的物体
+    public GameObject buildingSystem;
 
     private void Start()
     {
-        // 获取物体上的 Collider2D 组件
         myCollider = GetComponent<Collider2D>();
-
-        // 检查是否成功获取 Collider2D
         if (myCollider == null)
         {
             Logger.LogError("Collider2D not found!");
@@ -21,20 +18,14 @@ public class Resctricion : MonoBehaviour
 
     private void Update()
     {
-        // 获取鼠标在屏幕上的位置
         Vector2 mousePosition = Input.mousePosition;
-
-        // 将屏幕坐标转换为世界坐标
+        // Convert screen coordinates to world coordinates
         Vector2 worldMousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
-        // 检查鼠标位置是否与物体发生碰撞
+        // Check if mouse position collides with object
         bool isColliding = myCollider.OverlapPoint(worldMousePosition);
-
-        // 处理碰撞结果
         if (!isColliding)
         {
-            Logger.Log("collidingh");
-            // 获取 BuildingSystem 物体上的 Renderer 组件
             buildingSystem.SetActive(false);
         }
         else
